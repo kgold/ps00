1930d68cc477c8ac2089d6210706e9504d7b9bca
kgold <kevin.gold@gmail.com>
2015-07-29 16:37:32 -0400

    Fixed spacing in 3

--- 3.rkt
+++ 3.rkt
@@ -2,11 +2,11 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname |3|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 ; f->c : Number -> Number
-    ; GIVEN: a temperature in degrees Fahrenheit as an argument
-    ; RETURNS: the equivalent temperature in degrees Celsius.
-    ; Examples:
-    ; (f->c 32)  => 0
-    ; (f->c 100) => 37.77777777777777....
+; GIVEN: a temperature in degrees Fahrenheit as an argument
+; RETURNS: the equivalent temperature in degrees Celsius.
+; Examples:
+; (f->c 32)  => 0
+; (f->c 100) => 37.77777777777777....
 (define (f->c f)
   (* (- f 32) (/ 5 9)))
 (f->c 32)
