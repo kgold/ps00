27bcc97208b9d46d7a7f26f8426b6909591c6308
kgold <kevin.gold@gmail.com>
2016-01-11 16:26:13 -0500

    Added THOUSAND
    THOUSAND constant

--- 1.rkt
+++ 1.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+(define THOUSAND 1000)
 (define DAYS-IN-YEAR 366)
 (define HOURS-IN-DAY 24)
 (define MINUTES-IN-HOUR 60)
